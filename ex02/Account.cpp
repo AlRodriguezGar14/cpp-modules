57b54dbecfeffa0bea73f3f3560abd3fc13bc47f
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2024-05-19 02:34:56 +0200

    module00/ex02: Build: Print output for each action

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,10 +6,11 @@ int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals = 0;
 
-Account::Account(int initial_deposit) : _amount(initial_deposit) {
+Account::Account(int initial_deposit) : _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0) {
 	_accountIndex = _nbAccounts;
     _nbAccounts++;
     _totalAmount += initial_deposit;
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created" << std::endl;
 }
 
 Account::~Account() {
@@ -17,8 +18,6 @@ Account::~Account() {
     _nbAccounts--;
     _totalAmount -= _amount;
 }
-
-
 
 int Account::getNbAccounts() {
 	return _nbAccounts;
@@ -37,7 +36,7 @@ int Account::getNbWithdrawals() {
 }
 
 void	Account::displayStatus() const {
-	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created" << std::endl;
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";deposits:" << _nbDeposits << ";withdrawals:" << _nbWithdrawals << std::endl;
 }
 
 void Account::displayAccountsInfos() {
@@ -49,16 +48,19 @@ void Account::makeDeposit(int deposit) {
 	_totalAmount += deposit;
 	_nbDeposits++;
 	_totalNbDeposits++;
+	std::cout << "index:" << _accountIndex << ";p_amount:" << (_amount - deposit) << ";deposit:" << deposit << ";amount:" << _amount << ";nb_deposits:" << _nbDeposits << std::endl;
 }
 
 bool Account::makeWithdrawal(int withdrawal) {
 	if (_amount < withdrawal) {
+		std::cout << "index:" << _accountIndex << ";p_amount:" << _amount << ";withdrawal:refused" << std::endl;
 		return false;
 	}
 	_amount -= withdrawal;
 	_totalAmount -= withdrawal;
 	_nbWithdrawals++;
 	_totalNbWithdrawals++;
+	std::cout << "index:" << _accountIndex << ";p_amount:" << (_amount + withdrawal) << ";withdrawal:" << withdrawal << ";amount:" << _amount << ";nb_withdrawals:" << _nbWithdrawals << std::endl;
 	return true;
 }
 
