cd1d832cee0f165f3da526c94f9b58acd6846b23
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2024-05-22 01:20:23 +0200

    module01/ex05 - Build: project done; Harl complains a lot

---
+++ mod01/ex05/Harl.cpp
@@ -0,0 +1,56 @@
+#include "Harl.hpp"
+#include <cctype>
+
+void    Harl::debug(void) {
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void    Harl::info(void) {
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void    Harl::warning(void) {
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void    Harl::error(void) {
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+std::string toLower(std::string str) {
+
+	std::string lower = "";
+	for (std::string::size_type i = 0; i != str.length(); i++) {
+		if (std::isupper(str[i]))
+			lower += std::tolower(str[i]);
+		else
+			lower += str[i];
+	}
+	return lower;
+}
+
+int getIndex(std::string level) {
+
+	std::string levels[4] = {"debug", "info", "warning", "error"};
+	for (int i = 0; i != 5; i++) {
+		if (level == levels[i])
+			return i;
+	}
+	return -1;
+
+}
+
+void    Harl::complain(std::string level) {
+
+	void    (Harl::*complainLevel[4]) (void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	int i = getIndex(toLower(level));
+	if (i != -1)
+		(this->*(complainLevel[i]))();
+	else
+		std::cout << "Invalid complain level" << std::endl;
+}
+
+Harl::Harl() {};
+Harl::Harl(const Harl &h) { (void)h; };
+Harl& Harl::operator = (const Harl &h) { (void)h; return *this; };
+Harl::~Harl() {};
---
+++ mod01/ex05/main.cpp
@@ -0,0 +1,13 @@
+#include "Harl.hpp"
+
+int main() {
+
+	Harl h;
+
+	h.complain("debug");
+	h.complain("warn");
+	h.complain("warning");
+	h.complain("inFo");
+	h.complain("ERROR");
+	return 1;
+}
