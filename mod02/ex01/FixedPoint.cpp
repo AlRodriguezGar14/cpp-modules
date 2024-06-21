4fd6eb8d47883559f1135e4cc9caace688ab1940
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2024-06-21 19:51:23 +0200

    Build: Mod02, ex01 done     - Convert from int and float to fixed point and vice-versa     - Operator overload

---
+++ mod02/ex01/FixedPoint.cpp
@@ -0,0 +1,59 @@
+#include "FixedPoint.hpp"
+
+Fixed::Fixed() {
+	std::cout << "Default constructor called" << std::endl;
+	m_fixedPointValue = 0;
+}
+
+// input * 2 ^ (fractional bits)
+// is the same af shifting to the left fractional-bits-times (next function)
+Fixed::Fixed(const int t_input) {
+	std::cout << "Int constructor called" << std::endl;
+	if (t_input >= MAX_INT || t_input <= MIN_INT) {
+		std::cout << "Input is out of range, fixedPointValue set to 0" << std::endl;
+		m_fixedPointValue = 0;
+		return ;
+	}
+	m_fixedPointValue = round(t_input * (pow(2, m_fractionalBits)));
+}
+
+Fixed::Fixed(const float t_input) {
+	std::cout << "Float constructor called" << std::endl;
+	if (t_input >= MAX_INT || t_input <= MIN_INT) {
+		std::cout << "Input is out of range, fixedPointValue set to 0" << std::endl;
+		m_fixedPointValue = 0;
+		return ;
+	}
+	m_fixedPointValue = round(t_input * (1 << m_fractionalBits));
+}
+
+Fixed::Fixed(const Fixed& t_Fixed) {
+	std::cout << "Copy constructor called" << std::endl;
+	m_fixedPointValue = t_Fixed.m_fixedPointValue;
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed& Fixed::operator=(const Fixed& t_Fixed) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &t_Fixed) {
+		m_fixedPointValue = t_Fixed.m_fixedPointValue;
+	}
+	return *this;
+}
+
+float Fixed::toFloat() const {
+	return ((float)m_fixedPointValue * (pow(2, -m_fractionalBits)));
+}
+
+int Fixed::toInt() const {
+	return (round(m_fixedPointValue * (pow(2, -m_fractionalBits))));
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed& t_Fixed) {
+//	std::cout << "The fixed point number is: " << t_Fixed.m_fixedPointValue << std::endl;
+	os << t_Fixed.toFloat();
+	return os;
+}
---
+++ mod02/ex01/FixedPoint.hpp
@@ -0,0 +1,28 @@
+#ifndef FIXEDPOINT_HPP
+#define FIXEDPOINT_HPP
+
+#include <iostream>
+#define FRACTIONAL_BITS 8
+#define MAX_INT pow(2, (32 - FRACTIONAL_BITS)) - 1
+#define MIN_INT pow(2, -(32 - FRACTIONAL_BITS))
+
+class Fixed {
+public:
+    Fixed ();
+	Fixed(const int t_input);
+	Fixed(const float t_input);
+    Fixed (const Fixed &t_Fixed);
+    ~Fixed ();
+    Fixed& operator = (const Fixed &t_Fixed);
+	float toFloat(void) const;
+	int toInt(void) const;
+	friend std::ostream& operator<<(std::ostream& os, const Fixed& t_Fixed);
+
+private:
+	int									m_fixedPointValue;
+	static const int m_fractionalBits = FRACTIONAL_BITS;
+
+};
+
+#endif
+
